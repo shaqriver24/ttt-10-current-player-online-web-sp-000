339166e122dd431d63c501f7a8da73c5f56f10af
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-11 07:01:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,8 @@
 def turn_count(board)
   count = 0
   board.each do |space|
-    if space == "X" || space == "O"
-      count += 1
-    end
+    space == "X" || space == "O" ? count += 1
   end
+      
   count
 end
